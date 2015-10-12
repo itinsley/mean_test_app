5562d314dcac3ae0e58a3227272077f5eb21f1f8
Ian Tinsley <itinsley@gmail.com>
2015-10-12 16:36:50 +1100

    Clean out debug code

--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -5,32 +5,11 @@ var express = require('express')
 // sample api route
 router.get('/api/restaurants', function(req, res) {
   // use mongoose to get all restaurants in the database
-  // res.send("hello world..")
   Restaurant.find(function(err, restaurants) {
     if (err)
       res.send(err);
     res.json(restaurants);
   });
-
-
-  // Simple proof of connection to db
-  // var mongoose = require('mongoose');
-  // mongoose.connect('mongodb://localhost/stencil-dev');
-
-  // var restaurantSchema = mongoose.Schema({
-  //   name: String,
-  //   description: String
-  // });
-
-  // var db = mongoose.connection;
-  // db.on('error', console.error.bind(console, 'connection error:'));
-  // db.once('open', function (callback) {
-  //   var Restaurant = mongoose.model('Restaurant', restaurantSchema);
-  //   Restaurant.find(function (err, restaurants) {
-  //     if (err) return console.error(err);
-  //      res.json(restaurants);
-  //   });
-  // });
 
 });
 
