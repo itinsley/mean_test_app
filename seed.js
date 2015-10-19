3f92d336eb7ee2299ebc6c03b8aee4c7d75ce630
Ian Tinsley <itinsley@gmail.com>
2015-10-19 17:39:43 +1100

    Seed with dependent data
     - Users created first then inserted into Restaurants, still seems a
    bit messy.

--- seed.js
+++ seed.js
@@ -5,52 +5,51 @@ User = require('./app/models/user')
 var config = require('./config');
 var mongoose = require('mongoose');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+var cn = mongoose.connection;
+cn.on('error', console.error.bind(console, 'connection error:'));
 
+function createRestaurants(crCallback){
+  var userIds
+  async.series([
+    function(callback){
+      User.find({},function(err, us){
+        userIds = us.map(function(u){return u.id})
+        callback()
+      })
+    },
+    function(callback){
+      var restaurant = new Restaurant({"name": "ian best", "description": "the best", "userVotes": userIds});
+      restaurant.save(function (err, result) {
+        return crCallback();
+      });
+    }
+  ])
 
-function createRestaurants(){
-  var restaurant = new Restaurant({"name": "ian best", "description": "the best"});
-  restaurant.save(function (err, result) {
-    handleError(err);
-    console.log("saved" + result);
+}
+
+function createUser(userName, callback){
+  var user = new User({"name": userName})
+  user.save(callback);
+}
+
+function createUsers(callback){
+  userData = ['peter','john','paul','mary']
+  async.each(userData, createUser, completed=function(err){
     callback();
   });
 }
 
-function createUsers(){
-  userData = ['peter','john','paul','mary']
-  userData.forEach(function(userName){
-    var user = new User({"name": userName})
-    user.save(function (err, result) {
-      handleError(err);
-      callback();
-    });
-  })
-}
 
-//ASYNC TASKS
-tasks = []
-tasks.push(function(callback){ createRestaurants() });
-tasks.push(function(callback){ createUsers() });
-
-//Open db, then clear the data synchronously before running async tasks and closing db.
-db.once('open', function (callback) {
-  mongoose.connection.db.dropDatabase(function(err, result) {
-    handleError(err);
-  });
-
-  async.parallel(tasks, function(){
-    db.close()
-  });
-});
+async.series([
+  function(callback){ cn.once('open', callback) },
+  function(callback){ cn.db.dropDatabase(callback) },
+  createUsers,
+  createRestaurants,
+  function(callback){
+    cn.close()
+    callback();
+    console.log("Finished. Database closed")
+  }
+]);
 
 mongoose.connect(config.url);
-
-function handleError(err){
-  if (err)
-    console.log("EXCEPTION");
-    console.log(err);
-    process.exit();
-}
-
